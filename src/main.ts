a60d75c820ccd5d7f7918a77b7cb2af656402db5
Najib Mansour BitBucket <najib.mansour@axentech.io>
2025-06-22 21:23:33 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './core/application/app.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  const swagger_config = new DocumentBuilder()
+    .setTitle('v1')
+    .setDescription('The Nest Template API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swagger_config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
