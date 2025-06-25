7cb958f3c52193179d5f033bc7d3d8a9fa9abaed
Najib Mansour BitBucket <najib.mansour@axentech.io>
2025-06-25 19:53:22 +0300

    fixed user service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { EntityManager, Repository } from 'typeorm';
@@ -7,21 +7,34 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) userRep: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private readonly userRep: Repository<User>,
+  ) {}
 
-  async create(createUserDto: CreateUserDto) {}
-
-  findAll() {}
-
-  findOne(id: number) {}
-
-  findOneWithUsername(username: string) {}
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async create(user: CreateUserDto) {
+    this.userRep.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  findAll() {
+    this.userRep.find();
+  }
+
+  findOneById(user_id: number) {
+    this.userRep.findOneBy({ id: user_id });
+  }
+
+  findOneWithUsername(username: string) {
+    this.userRep.findOneBy({ username: username });
+  }
+
+  update(user_id: number, user: UpdateUserDto) {
+    this.userRep.update({ id: user_id }, user);
+
+    return `This action updates a #${user_id} user`;
+  }
+
+  remove(user_id: number) {
+    this.userRep.delete({ id: user_id });
+    return `This action removes a #${user_id} user`;
   }
 }
