e69f92108916f6e785a07279bf034d7505b4b031
Michael <michaeldebs16@gmail.com>
2025-06-26 21:37:19 +0300

    kss ema ma chtaghlit

--- src/core/database/data-source.ts
+++ src/core/database/data-source.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 import { DataSource } from "typeorm";
 
-dotenv.config();
+    dotenv.config();
 
 
 const {
--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -2,11 +2,23 @@ import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-import dataSource from './data-source';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataSource.options),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('POSTGRES_HOST'),
+        port: config.get('POSTGRES_PORT'),
+        username: config.get('POSTGRES_USER'),
+        password: config.get('POSTGRES_PASSWORD'),
+        database: config.get('POSTGRES_DB'),
+        entities: [
+          join(__dirname, '../../modules/**/entities/*.entity.{ts,js}'),
+        ],
+      }),
+    }),
   ],
 })
 export class DatabaseModule {}
